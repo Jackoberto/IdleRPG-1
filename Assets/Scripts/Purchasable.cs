4c44a06b9061a9ac21643468cd758e2c07ce9381
Jackoberto <jakar17@elev.fryshuset.se>
2020-11-06 15:55:35 +0100

    Revert "Revert "Changes some balance values""
    This reverts commit 7e592cf825d68aceae83c4b413c362dc59b6c495.

--- Assets/Scripts/Purchasable.cs
+++ Assets/Scripts/Purchasable.cs
@@ -5,30 +5,30 @@ using UnityEngine.UI;
 [Serializable]
 public class Purchasable {
 	public Text buttonLabel;
-	GoldProductionData goldProductionData;
+	ProductionData _productionData;
 	private Resource resource;
 	string productId;
 
-	bool IsAffordable => goldProductionData.costsResource.GoldAmount >= this.goldProductionData.GetActualCosts(this.Amount);
+	bool IsAffordable => _productionData.costsResource.GoldAmount >= this._productionData.GetActualCosts(this.Amount);
 
 	public int Amount {
-		get => PlayerPrefs.GetInt(this.goldProductionData.name+"_"+this.productId, 0);
-		private set => PlayerPrefs.SetInt(this.goldProductionData.name+"_"+this.productId, value);
+		get => PlayerPrefs.GetInt(this._productionData.name+"_"+this.productId, 0);
+		private set => PlayerPrefs.SetInt(this._productionData.name+"_"+this.productId, value);
 	}
 
-	public void SetUp(GoldProductionData goldProductionData, Resource resource, string productId) {
-		this.goldProductionData = goldProductionData;
+	public void SetUp(ProductionData productionData, Resource resource, string productId) {
+		this._productionData = productionData;
 		this.resource = resource;
 		this.productId = productId;
-		this.buttonLabel.text = $"Add {productId} for {goldProductionData.GetActualCosts(this.Amount)} {this.goldProductionData.costsResource.name}";
+		this.buttonLabel.text = $"Add {productId} for {productionData.GetActualCosts(this.Amount)} {this._productionData.costsResource.name}";
 	}
 
 	public void Purchase() {
 		if (!this.IsAffordable) 
 			return;
-		this.resource.GoldAmount -= this.goldProductionData.GetActualCosts(this.Amount);
+		this.resource.GoldAmount -= this._productionData.GetActualCosts(this.Amount);
 		this.Amount += 1;
-		this.buttonLabel.text = $"Add {this.productId} for {this.goldProductionData.GetActualCosts(this.Amount)} {this.resource.name}";
+		this.buttonLabel.text = $"Add {this.productId} for {this._productionData.GetActualCosts(this.Amount)} {this.resource.name}";
 	}
 
 	public void Update() => UpdateTextColor();
