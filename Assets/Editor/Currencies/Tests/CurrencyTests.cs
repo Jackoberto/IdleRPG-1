6e2d0efd8e12c739852a450e6effc21450eff2bb
Jackoberto <jakar17@elev.fryshuset.se>
2020-11-10 16:28:16 +0100

    Adds some unit testing with currencies

---
+++ Assets/Editor/Currencies/Tests/CurrencyTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Currencies.Tests
+{
+    public class CurrencyTests
+    {
+        [Test]
+        public void MultiplyDollar()
+        {
+            var five = new Dollar(5);
+            Assert.AreEqual(new Dollar(10), five.Times(2));
+            Assert.AreEqual(new Dollar(15), five.Times(3));
+        }
+        
+        [Test]
+        public void ToStringFormat()
+        {
+            Assert.AreEqual("5 Dollar", new Dollar(5).ToString());
+        }
+
+        [Test]
+        public void MultiplySEK()
+        {
+            var five = new SEK(5);
+            Assert.AreEqual(new SEK(10), five.Times(2));
+            Assert.AreEqual(new SEK(15), five.Times(3));
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+       /* [UnityTest]
+        public IEnumerator CurrencyTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }*/
+    }
+}
---
+++ Assets/Scripts/Currencies/SEK.cs
@@ -0,0 +1,28 @@
+namespace Currencies
+{
+    public class SEK
+    {
+        private int amount;
+        public SEK(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public SEK Times(int factor)
+        {
+            return new SEK(this.amount * factor);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.amount} {GetType().Name}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is SEK sek))
+                return false;
+            return sek.amount == this.amount;
+        }
+    }
+}
