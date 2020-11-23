24e724d8a309c68d214ded130f3092fa3c7f5dd8
Jackoberto <jakar17@elev.fryshuset.se>
2020-11-23 16:42:22 +0100

    Adds TimeClass that saves play time and difference between end and start

---
+++ Assets/Editor/Common/Tests/TimePlayedTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Globalization;
+using UnityEngine;
+
+namespace Common.Tests
+{
+    public class TimePlayedTests : MonoBehaviour
+    {
+        /*[Test]
+        public void TestConvertToInt()
+        {
+            Assert.NotZero(TimePlayed.ConvertToInt(DateTime.Parse("0050-11-23 15:20:30")));
+        }
+        
+        [Test]
+        public void TestConvertFromInt()
+        {
+            Assert.NotNull(TimePlayed.ConvertFromInt(1606145000));
+        }*/
+
+        [Test]
+        public void TestInitialize()
+        {
+            Assert.IsTrue(TimePlayed.Initialize());
+        }
+
+        [ContextMenu("Save")] [Test]
+        public void TestSave()
+        {
+            TimePlayed.SaveDestroyedTime();
+        }
+    }
+}
---
+++ Assets/Scripts/TimeTesting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using Common;
+
+public class TimeTesting : MonoBehaviour
+{
+    private void Start()
+    {
+        TimePlayed.Initialize();
+    }
+    
+    [ContextMenu("Update Time")]
+    private void DisplayTimePlayed()
+    {
+        Debug.Log(TimePlayed.GetTimePlayed());
+    }
+
+    private void OnDestroy()
+    {
+        TimePlayed.SaveDestroyedTime();
+        TimePlayed.SaveTimePlayed();
+    }
+}
